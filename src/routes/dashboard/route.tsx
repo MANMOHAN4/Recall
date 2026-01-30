042ebb87ec875f99f3cb4497ce926c01c5e6bc47
MANMOHAN4 <manmohanca42005@gmail.com>
2026-01-30 16:31:02 +0530

    Update dashboard route with loader loading the user session data

--- src/routes/dashboard/route.tsx
+++ src/routes/dashboard/route.tsx
@@ -6,15 +6,24 @@ import {
 } from '../../components/ui/sidebar'
 import { AppSidebar } from '../../components/web/app-sidebar'
 import { Separator } from '../../components/ui/separator'
+import { getSessionFn } from '../../data/session'
 
 export const Route = createFileRoute('/dashboard')({
   component: RouteComponent,
+  loader: async () => {
+    const session = await getSessionFn()
+
+    return {
+      user: session.user,
+    }
+  },
 })
 
 function RouteComponent() {
+  const { user } = Route.useLoaderData()
   return (
     <SidebarProvider>
-      <AppSidebar />
+      <AppSidebar user={user} />
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 px-4">
@@ -25,8 +34,8 @@ function RouteComponent() {
             />
           </div>
         </header>
-        <div className='flex flex-1 flex-col gap-4 p-4 pt-0'>
-            <Outlet />
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+          <Outlet />
         </div>
       </SidebarInset>
     </SidebarProvider>
