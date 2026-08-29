fa9ad15b1ba2a8d14109f3db5fabdc6139e455d9
MANMOHAN4 <manmohanca42005@gmail.com>
2026-08-29 18:58:49 +0530

    update

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,5 @@
 import { createAuthClient } from 'better-auth/react'
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: 'https://recall-c5rdohyws-manmohanca42005-5945.vercel.app',
+  baseURL: 'https://recall-pied-gamma.vercel.app',
 })
